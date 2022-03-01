eaf699fc338b5cb7fa43e4aab62152fd5a2bf760
Val <82764150+hopkee@users.noreply.github.com>
2022-03-01 03:32:34 +0300

    final

---
+++ ImageFetchApp/ImageFetchApp/Data/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFetchApp
+//
+//  Created by Valya on 1.03.22.
+//
+
+import UIKit
+
+public let imagesURLS = ["https://images.unsplash.com/photo-1640622307911-ee5870412ab5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+                  "https://imags.unsplash.com/photo-1646061632529-b552ea07b50b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+                  "https://images.unsplash.com/photo-1646062142799-73dd49cf315a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+                  "https://images.unsplash.com/photo-1646053995253-b78df230b739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3132&q=80",
+                  "https://images.unsplash.com/photo-1645908107147-b3d0d7e37ad8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80"]
